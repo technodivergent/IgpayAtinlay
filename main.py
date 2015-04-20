9eea21502b4a72b4de1b327c3aef508e82187f66
George Hall <technikalmind@gmail.com>
2015-04-20 01:12:27 -0400

    Added support for sentences
    Users can submit sentences for translation now

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-vowels = 'aeiou';
-def PigLatin(input):
+vowels = 'AEIOUaeiou';
+def PigWord(input):
 	"Print input in pig latin"
 	if input == "exit":
-		return "Ankthay ouya orfay usingway Igpay Atinlay Anslatortray!";
+		return "Ankthay ouyay orfay usingway Igpay Atinlay Anslatortray!";
 	elif input[0] in vowels:
+		# if first letter is a vowel, just append a postfix
 		return ''.join(input) + "way";
 	else:
+		# find out where the first vowel is indexed
+		# then use that to slice the input into
+		# a beginning and end. rearrange them and
+		# append a postfix.
 		index = firstVowel(input);
 		engBegin = input[0:index];
 		engEnd = input[index:];
-		pigLatin = engEnd + engBegin + "ay";
-		return pigLatin;
+		pigWord = engEnd + engBegin + "ay";
+		return pigWord;
 				
 def firstVowel(input):
 	"Returns index location of first vowel"
@@ -23,9 +28,19 @@ def firstVowel(input):
 			break;
 			
 print "Welcome to the Igpay Atinlay Anslatortray";
-print ""
 
+# Allow user to translate until program is closed or exited
 while input != "exit":
-	input = raw_input("What would you like translated? ");
-	print PigLatin(input);
+	input = raw_input("\nWhat would you like translated? ");
+	inputList = input.split(); # in case the user inputs a sentence, split each word into a list
+	
+	# iterate through the list of words
+	for words in inputList:
+		# to prevent 'Latin' -> 'atinLay' the input should be lowercase.
+		# then capitalize the output: 'Atinlay'
+		# otherwise, just process the word as normal
+		if words[0].isupper():
+			print PigWord(words.lower()).title(),
+		else:
+			print PigWord(words),
 
